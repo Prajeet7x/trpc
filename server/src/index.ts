50f5c6e845041adcb996e0f4db016cbeaeb7a0ac
Prajeet <prajeet911@gmail.com>
2024-01-18 17:52:16 +0545

    Feat: ✨ Project setup

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Express Server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
