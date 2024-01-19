f65df42a3bf4a04a63353baef365baba16764dd8
Prajeet <prajeet911@gmail.com>
2024-01-19 09:18:33 +0545

    Fix: ✨ Ports and scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,24 @@
 // src/index.ts
-import express from 'express';
+import express from "express";
+
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+const tServer = initTRPC.create();
+
+const router = tServer.router({
+  helloWorld: tServer.procedure.query(() => {
+    return "Hi";
+  }),
+});
 
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript Express Server!');
+app.use("/trpc", createExpressMiddleware({router: router}));
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript Express Server!");
 });
 
 app.listen(port, () => {
